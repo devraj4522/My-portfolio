88389b04afefb333de31e657a47b463d4845fd35
Dev Raj <rehansingh.4522@gmail.com>
2020-11-29 17:57:29 +0530

    Updates

---
+++ precache-manifest.27c004480d308926a77fbdeef01e5a53.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39c25b2adfbe60fbd5acf697b92007ed",
+    "url": "/Devraj-personal-site/index.html"
+  },
+  {
+    "revision": "aafe1b69389e46722340",
+    "url": "/Devraj-personal-site/static/css/3.879e9400.chunk.css"
+  },
+  {
+    "revision": "86a9301aa5629ec5c9e6",
+    "url": "/Devraj-personal-site/static/css/4.54df59d4.chunk.css"
+  },
+  {
+    "revision": "cc46cebbe8619a53c0cc",
+    "url": "/Devraj-personal-site/static/css/main.12ae576a.chunk.css"
+  },
+  {
+    "revision": "4f4a7bef252a2daf4ca2",
+    "url": "/Devraj-personal-site/static/js/2.29f97526.chunk.js"
+  },
+  {
+    "revision": "aafe1b69389e46722340",
+    "url": "/Devraj-personal-site/static/js/3.9a227dcc.chunk.js"
+  },
+  {
+    "revision": "86a9301aa5629ec5c9e6",
+    "url": "/Devraj-personal-site/static/js/4.365ca45e.chunk.js"
+  },
+  {
+    "revision": "cc46cebbe8619a53c0cc",
+    "url": "/Devraj-personal-site/static/js/main.ebe3d383.chunk.js"
+  },
+  {
+    "revision": "60ce8b87745ca1f0543d",
+    "url": "/Devraj-personal-site/static/js/runtime-main.5a4fcbe4.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/Devraj-personal-site/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/Devraj-personal-site/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c59720d63dc3595a12e1c7e641a1bfa5",
+    "url": "/Devraj-personal-site/static/media/airbnbLogo.c59720d6.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/Devraj-personal-site/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/Devraj-personal-site/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/Devraj-personal-site/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "8ddf76a14a2e3ad3ba62b46d49a75a74",
+    "url": "/Devraj-personal-site/static/media/facebookLogo.8ddf76a1.png"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/Devraj-personal-site/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "83a05b10639d238bd8c5bbab279ab911",
+    "url": "/Devraj-personal-site/static/media/harvardLogo.83a05b10.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Devraj-personal-site/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/Devraj-personal-site/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/Devraj-personal-site/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/Devraj-personal-site/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "4b2938b9f5ea2f1ddbd45dccaff628bc",
+    "url": "/Devraj-personal-site/static/media/quoraLogo.4b2938b9.png"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/Devraj-personal-site/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/Devraj-personal-site/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "3b758ef56d398145542e137daa28e392",
+    "url": "/Devraj-personal-site/static/media/stanfordLogo.3b758ef5.png"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/Devraj-personal-site/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
